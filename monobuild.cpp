2f668e2fcb696929a5fe0413c7041ab46e9dc5cd
Michael Pidde <michaelpidde@fastmail.com>
2024-11-05 21:31:29 -0600

    Remove monobuild. Add gitignore

--- monobuild.cpp
+++
@@ -1,8 +0,0 @@
-#include "libs/imgui/backends/imgui_impl_sdl2.cpp"
-#include "libs/imgui/backends/imgui_impl_sdlrenderer2.cpp"
-#include "libs/imgui/imgui.cpp"
-#include "libs/imgui/imgui_draw.cpp"
-#include "libs/imgui/imgui_widgets.cpp"
-#include "libs/imgui/imgui_tables.cpp"
-
-//#include "libs/imgui/imgui_demo.cpp"
